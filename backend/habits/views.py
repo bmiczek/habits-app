6224a3cc93e66bd9c131d6c51693ac57144b0a10
Bart Miczek <bmiczek@gmail.com>
2020-02-27 06:15:04 -0800

    add auth

--- backend/habits/views.py
+++ backend/habits/views.py
@@ -1,30 +1,35 @@
+from rest_framework.permissions import IsAuthenticated
+
 from backend.habits.models import Habit, Entry
 from backend.habits.serializers import HabitSerializer, EntrySerializer
 from rest_framework import generics
 
 
 class HabitList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
 
 
 class HabitDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
 
 
 class EntryList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = EntrySerializer
 
     def get_queryset(self):
-        habit = self.kwargs['habit_pk']
+        habit = self.kwargs["habit_pk"]
         return Entry.objects.filter(habit=habit)
 
 
 class EntryDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = EntrySerializer
 
     def get_queryset(self):
-        habit = self.kwargs['habit_pk']
+        habit = self.kwargs["habit_pk"]
         return Entry.objects.filter(habit=habit)
-
